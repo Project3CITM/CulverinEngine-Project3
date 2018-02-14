828f7cda2ed7bae719403f4d266284742a77c995
elliotjb <elliot.jimenez1@gmail.com>
2018-02-14 19:32:58 +0100

    Scripting & Movement: Add functions in Map.cs

--- TestUsing/TestUsing/TestMovement.cs
+++ TestUsing/TestUsing/TestMovement.cs
@@ -3,56 +3,47 @@ using CulverinEditor.Debug;
 using CulverinEditor.Map;
 
 //Attach this script to the tank parent object if you want to see it rotate
-public class TestMovement
+public class TestMovement : CulverinBehaviour
 {
     private float movSpeed = 0.0f;
     Vector3 endPosition;
     int distanceToMove = 10;
-
+    int[,] array2Da;
+    int map_width = 0;
+    int map_height = 0;
 
     void Start()
     {
-        int[,] array2Da;//
-        array2Da = new int[5, 5];
-        for (int i = 0; i < 5; i++)
+        map_width = Map.GetWidthMap();
+        map_height = Map.GetHeightMap();
+        array2Da = new int[map_width, map_height];
+        for (int i = 0; i < map_width; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < map_height; j++)
             {
                 array2Da[i, j] = 0;
             }
         }
-        array2Da[0,0] = 1;
-        Debug.Log(array2Da[0,0].ToString());
-        endPosition = GameObject.gameObject.GetComponent<Transform>().Position;
+        //array2Da[0,0] = 1;
+        //Debug.Log(array2Da[0,0].ToString());
+        endPosition = GetComponent<Transform>().Position;
         string map = Map.GetMapString();
         Debug.Log(map);
         int t = 0;
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < map_width; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < map_height; j++)
             {
                 array2Da[i, j] = int.Parse(map[t].ToString());
                 t += 1;
-            }
-        }
-        Debug.Log(array2Da[0, 0].ToString());
-        Debug.Log(array2Da[1, 1].ToString());
-        Debug.Log(array2Da[1, 0].ToString());
-        Debug.Log(array2Da[0, 1].ToString());
-        Debug.Log("Map?");
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                Debug.Log(array2Da[i, j].ToString());
             }
         }
     }
 
     void Update()
     {
-        GameObject.gameObject.GetComponent<Transform>().Position = Vector3.MoveTowards(GameObject.gameObject.GetComponent<Transform>().Position, endPosition, movSpeed * Time.DeltaTime());
-        if (GameObject.gameObject.GetComponent<Transform>().Position == endPosition)
+        GetComponent<Transform>().Position = Vector3.MoveTowards(GetComponent<Transform>().Position, endPosition, movSpeed * Time.DeltaTime());
+        if (GetComponent<Transform>().Position == endPosition)
         {
             if (Input.KeyDown("A")) //Left
             {
