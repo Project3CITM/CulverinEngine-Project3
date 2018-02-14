63dcc42aef56fb09fea1a0687c8130fb787bb065
Joan Pareja <imbecilustotalus@gmail.com>
2018-02-14 20:48:50 +0100

    Test movement updated

--- TestUsing/TestUsing/TestMovement.cs
+++ TestUsing/TestUsing/TestMovement.cs
@@ -9,6 +9,8 @@ public class TestMovement : CulverinBehaviour
     Vector3 endPosition;
     int distanceToMove = 10;
     int[,] array2Da;
+    int curr_x = 0;
+    int curr_y = 0;
     int map_width = 0;
     int map_height = 0;
 
@@ -38,6 +40,20 @@ public class TestMovement : CulverinBehaviour
                 t += 1;
             }
         }
+
+        //Search player position
+        for (int i = 0; i < map_width; i++)
+        {
+            for (int j = 0; j < map_height; j++)
+            {
+                if (array2Da[i, j] == 3)
+                {
+                    curr_x = i;
+                    curr_y = j;
+                    array2Da[i, j] = 1;
+                }
+            }
+        }
     }
 
     void Update()
@@ -45,21 +61,34 @@ public class TestMovement : CulverinBehaviour
         GetComponent<Transform>().Position = Vector3.MoveTowards(GetComponent<Transform>().Position, endPosition, movSpeed * Time.DeltaTime());
         if (GetComponent<Transform>().Position == endPosition)
         {
-            if (Input.KeyDown("A")) //Left
+            //2D coordinates, y=z in 3D coordinates
+            int tile_mov_x = 0;
+            int tile_mov_y = 0;
+
+
+            if (Input.KeyDown(KeyCode.A)) //Left
             {
-                endPosition = new Vector3(endPosition.x + distanceToMove, endPosition.y, endPosition.z);
+                tile_mov_x =  - 1;
             }
-            else if (Input.KeyDown("D")) //Right
+            else if (Input.KeyDown(KeyCode.D)) //Right
             {
-                endPosition = new Vector3(endPosition.x - distanceToMove, endPosition.y, endPosition.z);
+                tile_mov_x = 1;
             }
-            else if (Input.KeyDown("W")) //Up
+            else if (Input.KeyDown(KeyCode.W)) //Up
             {
-                endPosition = new Vector3(endPosition.x, endPosition.y, endPosition.z + distanceToMove);
+                tile_mov_y = 1;
             }
-            else if (Input.KeyDown("S")) //Down
+            else if (Input.KeyDown(KeyCode.S)) //Down
             {
-                endPosition = new Vector3(endPosition.x, endPosition.y, endPosition.z - distanceToMove);
+                tile_mov_y = - 1;
+            }
+
+            //Calculate endPosition
+            if ((tile_mov_x != 0 || tile_mov_y != 0) && array2Da[curr_x + tile_mov_x, curr_y + tile_mov_y] == 1)
+            {
+                endPosition = new Vector3(GetComponent<Transform>().Position.x + distanceToMove * tile_mov_x, GetComponent<Transform>().Position.y, GetComponent<Transform>().Position.z + distanceToMove * tile_mov_y);
+                curr_x += tile_mov_x;
+                curr_y += tile_mov_y;
             }
         }
     }
