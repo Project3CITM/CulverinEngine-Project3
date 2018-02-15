b97411b24456a95a535225ebeeb0369cd58f225f
Carlos Cabreira Sánchez <carcasanchez@gmail.com>
2018-02-15 10:02:27 +0100

    Audio: Added some functions to C#

---
+++ CulverinEditor/CulverinEditor/Audio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace CulverinEditor
+{
+    public class Audio
+    {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+       public static extern void StopSounds();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void PauseSounds();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void ResumeSounds();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void SetAudioVariableValue(string variable_name, float value);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void externChangeState(string group, string new_state);
+    }
+}
--- Engine/ModuleAudio.h
+++ Engine/ModuleAudio.h
@@ -42,11 +42,10 @@ public:
 	void StopSounds();
 	void PauseSounds();
 	void ResumeSounds();
-
-	void SetListener(CompAudio* c);
 	void SetAudioVariableValue(const char* variable_name, float value);
 	void ChangeState(const char* group, const char* new_state);
 
+	void SetListener(CompAudio* c);
 
 
 private:
