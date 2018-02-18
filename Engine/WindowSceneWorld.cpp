03bcb7c99033845b158bf6ac1786ad78733ef921
mizquierdo97 <mizquierdo97@gmail.com>
2018-02-18 20:21:14 +0100

    ImGuizmo: Fixed Bug

--- Engine/WindowSceneWorld.cpp
+++ Engine/WindowSceneWorld.cpp
@@ -4,7 +4,7 @@
 #include "ImGui/ImGuizmo.h"
 #include "ModuleCamera3D.h"
 #include "Scene.h"
-
+#include "ImGui\ImGuizmo.h"
 #define PICK_OFFSET_Y -15
 
 SceneWorld::SceneWorld() : WindowManager()
@@ -54,19 +54,21 @@ void SceneWorld::ShowSceneWorld()
 	//LOG("X:%f Y:%f W:%f H:%f", SceneWindow.x, SceneWindow.y, SceneWindow.z, SceneWindow.w);
 	ImGuizmo::SetDrawlist();
 	// Generate mouse ray ---------------------------
-	if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LALT) != KEY_REPEAT)
-	{
-		mouse_pos.x = ImGui::GetMousePos().x - scene_window.x;
-		mouse_pos.y = ImGui::GetMousePos().y - scene_window.y;
-
-		if (mouse_pos.x >= 0 && mouse_pos.y >= 0 &&
-			mouse_pos.x <= scene_window.z && mouse_pos.y <= scene_window.w && ImGui::IsMouseHoveringWindow())
+	bool test = ImGuizmo::IsOver();
+	if (!test) {
+		if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LALT) != KEY_REPEAT)
 		{
-			App->camera->MousePick(mouse_pos.x, mouse_pos.y + PICK_OFFSET_Y, scene_window.z, scene_window.w);
-			//LOG("MOUSE CLICK (%f, %f).", mouse_pos.x, mouse_pos.y);
+			mouse_pos.x = ImGui::GetMousePos().x - scene_window.x;
+			mouse_pos.y = ImGui::GetMousePos().y - scene_window.y;
+
+			if (mouse_pos.x >= 0 && mouse_pos.y >= 0 &&
+				mouse_pos.x <= scene_window.z && mouse_pos.y <= scene_window.w && ImGui::IsMouseHoveringWindow())
+			{
+				App->camera->MousePick(mouse_pos.x, mouse_pos.y + PICK_OFFSET_Y, scene_window.z, scene_window.w);
+				//LOG("MOUSE CLICK (%f, %f).", mouse_pos.x, mouse_pos.y);
+			}
 		}
 	}
-
 	App->camera->can_move_cam = ImGui::IsMouseHoveringWindow(); //TODO ELLIOT CHange to variable in WindowManager.h
 	
 	ImGui::Image((void*)App->scene->scene_buff->GetTexture(), ImGui::GetContentRegionAvail(), ImVec2(0, 1), ImVec2(1, 0)); // Set texture to buffer
