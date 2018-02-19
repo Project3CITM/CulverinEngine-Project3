cb92010f3ab155a4eb2ca7d8466076bd580d48a2
elliotjb <elliot.jimenez1@gmail.com>
2018-02-19 09:29:34 +0100

    Movement: Fixed error with TestMovement.cs

--- TestUsing/TestUsing/TestMovement.cs
+++ TestUsing/TestUsing/TestMovement.cs
@@ -20,40 +20,40 @@ public class TestMovement : CulverinBehaviour
         map_width = Map.GetWidthMap();
         map_height = Map.GetHeightMap();
         array2Da = new int[map_width, map_height];
-        for (int i = 0; i < map_width; i++)
+        for (int y = 0; y < map_height; y++)
         {
-            for (int j = 0; j < map_height; j++)
+            for (int x = 0; x < map_width; x++)
             {
-                array2Da[i, j] = 0;
+                array2Da[x, y] = 0;
             }
         }
         //array2Da[0,0] = 1;
         //Debug.Log(array2Da[0,0].ToString());
-        endPosition = GetComponent<Transform>().position;
-        endRotation = transform.Rotation;
+        endPosition = GetComponent<Transform>().local_position;
+        endRotation = transform.local_rotation;
 
         string map = Map.GetMapString();
         Debug.Log(map);
         int t = 0;
-        for (int i = 0; i < map_width; i++)
+        for (int y = 0; y < map_height; y++)
         {
-            for (int j = 0; j < map_height; j++)
+            for (int x = 0; x < map_width; x++)
             {
-                array2Da[i, j] = int.Parse(map[t].ToString());
+                array2Da[x, y] = int.Parse(map[t].ToString());
                 t += 1;
             }
         }
 
         //Search player position
-        for (int i = 0; i < map_width; i++)
+        for (int y = 0; y < map_height; y++)
         {
-            for (int j = 0; j < map_height; j++)
+            for (int x = 0; x < map_width; x++)
             {
-                if (array2Da[i, j] == 3)
+                if (array2Da[x, y] == 2)
                 {
-                    curr_x = i;
-                    curr_y = j;
-                    array2Da[i, j] = 1;
+                    curr_x = x;
+                    curr_y = y;
+                    array2Da[x, y] = 0;
                 }
             }
         }
@@ -70,12 +70,12 @@ public class TestMovement : CulverinBehaviour
 
             if (rot_x != 0)
             {
-                transform.Rotation = new Vector3(transform.Rotation.x - rot_x, transform.Rotation.y, 0);
+                transform.local_rotation = new Vector3(transform.local_rotation.x - rot_x, transform.local_rotation.y, 0);
             }
 
             if (rot_y != 0)
             {
-                transform.Rotation = new Vector3(transform.Rotation.x, transform.Rotation.y + rot_y, 0);
+                transform.local_rotation = new Vector3(transform.local_rotation.x, transform.local_rotation.y + rot_y, 0);
             }
         }
         else
@@ -91,7 +91,7 @@ public class TestMovement : CulverinBehaviour
             Vector3 backwards = new Vector3(0, 180, 0);
             Vector3 leftwards = new Vector3(0, 270, 0);
 
-            float curr_rot_y = transform.Rotation.y;
+            float curr_rot_y = transform.local_rotation.y;
 
             if (curr_rot_y < 45 || curr_rot_y > 315)
             {
@@ -111,7 +111,7 @@ public class TestMovement : CulverinBehaviour
             }
         }
 
-        if (GetComponent<Transform>().position == endPosition && transform.Rotation == endRotation)
+        if (GetComponent<Transform>().local_position == endPosition && transform.local_rotation == endRotation)
         {
             //2D coordinates, y=z in 3D coordinates
             int tile_mov_x = 0;
@@ -120,11 +120,11 @@ public class TestMovement : CulverinBehaviour
 
             if (Input.GetKeyDown(KeyCode.Q)) //Left
             {
-                endRotation = new Vector3(transform.Rotation.x, transform.Rotation.y - 90, 0);
+                endRotation = new Vector3(transform.local_rotation.x, transform.local_rotation.y - 90, 0);
             }
             if (Input.GetKeyDown(KeyCode.E)) //Left
             {
-                endRotation = new Vector3(transform.Rotation.x, transform.Rotation.y + 90, 0);
+                endRotation = new Vector3(transform.local_rotation.x, transform.local_rotation.y + 90, 0);
             }
 
             if (Input.GetKeyDown(KeyCode.A)) //Left
@@ -145,16 +145,16 @@ public class TestMovement : CulverinBehaviour
             }
 
             //Calculate endPosition
-            if ((tile_mov_x != 0 || tile_mov_y != 0) && array2Da[curr_x + tile_mov_x, curr_y + tile_mov_y] == 1)
+            if ((tile_mov_x != 0 || tile_mov_y != 0) && array2Da[curr_x + tile_mov_x, curr_y + tile_mov_y] == 0)
             {
-                endPosition = new Vector3(GetComponent<Transform>().position.x + distanceToMove * tile_mov_x, GetComponent<Transform>().position.y, GetComponent<Transform>().position.z + distanceToMove * tile_mov_y);
+                endPosition = new Vector3(GetComponent<Transform>().local_position.x + distanceToMove * tile_mov_x, GetComponent<Transform>().local_position.y, GetComponent<Transform>().local_position.z + distanceToMove * tile_mov_y);
                 curr_x += tile_mov_x;
                 curr_y += tile_mov_y;
             }
         }
         else
         {
-            GetComponent<Transform>().position = Vector3.MoveTowards(GetComponent<Transform>().position, endPosition, movSpeed * Time.DeltaTime());
+            GetComponent<Transform>().local_position = Vector3.MoveTowards(GetComponent<Transform>().local_position, endPosition, movSpeed * Time.DeltaTime());
         }
     }
 }
