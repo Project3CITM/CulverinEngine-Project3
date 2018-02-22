5e4b4a9368a86f805c6885b4bb4b728efe2b8b5f
elliotjb <elliot.jimenez1@gmail.com>
2018-02-22 14:40:32 +0100

    Scripting: Test code,.cs

--- TestUsing/TestUsing/Code.cs
+++ TestUsing/TestUsing/Code.cs
@@ -3,9 +3,21 @@ using CulverinEditor.Debug;
 
 public class Code : CulverinBehaviour
 {
-    void OnTriggerEnter()
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update()
     {
-        CompAudio audio = GetComponent<CompAudio>();
-        audio.PlayEvent("SwordSlash");
+
+        if (Input.GetMouseButtonRepeat(2))
+        {
+            yaw = speedH * Input.GetMouseMoutionX();
+            pitch = speedV * Input.GetMouseMoutionY();
+            transform.RotateAroundAxis(Vector3.Up, yaw * 5 * Time.DeltaTime());
+            transform.RotateAroundAxis(Vector3.Right, pitch * 5 * Time.DeltaTime());
+        }
     }
 }
