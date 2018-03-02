d163bf6b7bc5bb36fd278c11fc7e04d6ff9c064b
Andreu <Andreu.rbj@gmail.com>
2018-03-02 11:52:15 +0100

    UI: Create ProcessEventAction on InputAction
    Co-Authored-By: nicobabot <nicobabot@users.noreply.github.com>

--- Engine/InputAction.h
+++ Engine/InputAction.h
@@ -14,7 +14,7 @@ enum Keystateaction
 
 enum ActionInputType
 {
-	UNKNOWN_ACTION=-1,
+	UNKNOWN_ACTION = -1,
 	AXIS_ACTION,
 	CONTROLLER_AXIS_ACTION,
 	KEY_ACTION,
@@ -26,7 +26,7 @@ enum ActionInputType
 
 enum AxisDirectionController
 {
-	NON_DIRECTION_C=-1,
+	NON_DIRECTION_C = -1,
 	UP_DIRECTION_C,
 	DOWN_DIRECTION_C,
 	RIGHT_DIRECTION_C,
@@ -41,19 +41,37 @@ public:
 	InputAction(const char* name, KeyRelation* key_relation, ActionInputType action_type = ActionInputType::UNKNOWN_ACTION);
 	~InputAction();
 
+	virtual bool ProcessEventAction(SDL_Event * input_event) { return false; }
+
 public:
 
 	std::string name = "";
 	ActionInputType action_type = ActionInputType::UNKNOWN_ACTION;
-	KeyRelation *key_relation=nullptr;
+	KeyRelation *key_relation = nullptr;
 
 };
 
 
-class AxisAction: public InputAction
+class AxisAction : public InputAction
 {
 public:
-	AxisAction(){ action_type = ActionInputType::AXIS_ACTION; }
+	AxisAction() { action_type = ActionInputType::AXIS_ACTION; }
+	bool ProcessEventAction(SDL_Event * input_event)
+	{
+		if (this->key_relation->key_type == KeyBindingType::MOUSE_AXIS_DEVICE)
+		{
+			if (input_event->type == SDL_MOUSEMOTION)
+			{
+				position.x = input_event->motion.x / SCREEN_SIZE;
+				position.y = input_event->motion.y / SCREEN_SIZE;
+
+				motion.x = input_event->motion.x;
+				motion.y = input_event->motion.y;
+				return true;
+			}
+		}
+		return false;
+	}
 	float2 position;
 	float2 motion;
 
@@ -63,15 +81,43 @@ class ControllerAxisAction : public InputAction
 {
 public:
 	ControllerAxisAction() { action_type = ActionInputType::CONTROLLER_AXIS_ACTION; }
+	bool ProcessEventAction(SDL_Event * input_event)
+	{
+		if (this->key_relation->key_type == KeyBindingType::CONTROLLER_AXIS_DEVICE)
+		{
+			if (input_event->type == SDL_CONTROLLERAXISMOTION)
+			{
+				direction_axis = input_event->caxis.value;
+				return true;
+			}
+		}
+		return false;
+	}
 	float2 motion;
 	AxisDirectionController direction = AxisDirectionController::NON_DIRECTION_C;
-
+	int direction_axis = 0;
 };
 
 class MouseButtonAction : public InputAction
 {
 public:
 	MouseButtonAction() { action_type = ActionInputType::MOUSE_BUTTON_ACTION; }
+
+	bool ProcessEventAction(SDL_Event * input_event)
+	{
+		if (this->key_relation->key_type == KeyBindingType::MOUSE_BUTTON_DEVICE)
+		{
+			if (input_event->type == SDL_MOUSEBUTTONDOWN)
+			{
+				state = Keystateaction::KEY_DOWN_ACTION;
+				number_clicks++;
+				return true;
+			}
+
+		}
+		return false;
+	}
+
 	Keystateaction state = Keystateaction::KEY_IDLE_ACTION;
 	int number_clicks = 0;
 };
@@ -80,10 +126,26 @@ class KeyAction : public InputAction
 {
 public:
 	KeyAction() { action_type = ActionInputType::KEY_ACTION; }
-	Keystateaction state = Keystateaction::KEY_IDLE_ACTION;
+
 	bool OnClick() { return state == Keystateaction::KEY_DOWN_ACTION; };
 	bool OnRelease() { return state == Keystateaction::KEY_UP_ACTION; };
 	bool OnRepeat() { return state == Keystateaction::KEY_REPEAT_ACTION; };
+
+	bool ProcessEventAction(SDL_Event * input_event)
+	{
+		if (this->key_relation->key_type == KeyBindingType::KEYBOARD_DEVICE)
+		{
+			if (input_event->type == SDL_KEYDOWN)
+			{
+				state = Keystateaction::KEY_DOWN_ACTION;
+				return true;
+			}
+
+		}
+		return false;
+	}
+
+	Keystateaction state = Keystateaction::KEY_IDLE_ACTION;
 
 };
 
@@ -91,11 +153,26 @@ class ButtonAction : public InputAction
 {
 public:
 	ButtonAction() { action_type = ActionInputType::BUTTON_ACTION; }
-	Keystateaction state = Keystateaction::KEY_IDLE_ACTION;
+
 	bool OnClick() { return state == Keystateaction::KEY_DOWN_ACTION; };
 	bool OnRelease() { return state == Keystateaction::KEY_UP_ACTION; };
 	bool OnRepeat() { return state == Keystateaction::KEY_REPEAT_ACTION; };
 
+	bool ProcessEventAction(SDL_Event * input_event)
+	{
+		if (this->key_relation->key_type == KeyBindingType::KEYBOARD_DEVICE)
+		{
+			if (input_event->type == SDL_KEYDOWN)
+			{
+				state = Keystateaction::KEY_DOWN_ACTION;
+				return true;
+			}
+
+		}
+		return false;
+	}
+
+	Keystateaction state = Keystateaction::KEY_IDLE_ACTION;
 };
 
 
@@ -103,4 +180,3 @@ public:
 
 
 #endif
-
