b481aa43a32ada8a764145fb1c8147cef1367ffc
mizquierdo97 <mizquierdo97@gmail.com>
2018-03-13 19:55:50 +0100

    DELETED ALL VS2 ASSETS

--- Engine/Game/Assets/TestLoad.cs
+++
@@ -1,8 +0,0 @@
-﻿using CulverinEditor;
-using CulverinEditor.SceneManagement;
-
-
-class TestLoad : CulverinBehaviour
-{
-   
-}
--- Engine/Game/Assets/TheonWeapon_Right.cs
+++
@@ -1,9 +0,0 @@
-﻿using CulverinEditor;
-using CulverinEditor.Debug;
-
-// The concept is to have WeaponController as a base class, and override for all the weapons
-
-public class TheonWeapon_Right : WeaponController
-{
-    
-}
