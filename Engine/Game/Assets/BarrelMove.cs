a4303d58a0c5b4093a5009b8e317b9d703ce371a
mizquierdo97 <mizquierdo97@gmail.com>
2018-03-15 18:17:36 +0100

    Fixed Lights bug

---
+++ Engine/Game/Assets/BarrelMove.cs
@@ -0,0 +1,40 @@
+﻿using CulverinEditor;
+using CulverinEditor.Debug;
+
+public class BarrelMove : CulverinBehaviour
+{
+    
+    GameObject parent_obj;
+    Transform trans;
+
+    public float final_pos_x;
+    public float final_pos_y;
+    public float final_pos_z;
+
+    public float movSpeed;
+
+    public float fall_speed;
+    public float fall_dist;
+
+    bool arrive = false;
+
+    public bool stop = false;
+    void Start()
+    {
+        arrive = false;
+        trans = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        float dt = Time.deltaTime;
+
+        Vector3 diff = trans.local_position - new Vector3(final_pos_x, final_pos_y, final_pos_z);
+        diff = diff.Normalized;
+
+        trans.local_position = diff * movSpeed * dt;
+
+
+
+    }
+
+}
