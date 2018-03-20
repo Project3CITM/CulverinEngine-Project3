1745e2bb37301b8ac687c98ca2d452b7dd69c889
Elliot Jimenez Bosch <elliot.jimenez1@gmail.com>
2018-03-20 16:20:49 +0100

    Script: finish script levelMap

--- CulverinEditor/CulverinEditor/LevelMap.cs
+++ CulverinEditor/CulverinEditor/LevelMap.cs
@@ -7,21 +7,29 @@ using CulverinEditor.Map;
 
 public class LevelMap : CulverinBehaviour
 {
-    public int[,] map;
+    private int[,] level_map;
     public int map_width = 0;
     public int map_height = 0;
+
+    public int[,] map
+    {
+        get
+        {
+            return level_map;
+        }
+    }
 
     void Start()
     {
         map_width = Map.GetWidthMap();
         map_height = Map.GetHeightMap();
 
-        map = new int[map_width, map_height];
+        level_map = new int[map_width, map_height];
         for (int y = 0; y < map_height; y++)
         {
             for (int x = 0; x < map_width; x++)
             {
-                map[x, y] = 0;
+                level_map[x, y] = 0;
             }
         }
 
@@ -32,7 +40,7 @@ public class LevelMap : CulverinBehaviour
         {
             for (int x = 0; x < map_width; x++)
             {
-                map[x, y] = int.Parse(map_string[t].ToString());
+                level_map[x, y] = int.Parse(map_string[t].ToString());
                 t += 1;
             }
         }
@@ -44,9 +52,31 @@ public class LevelMap : CulverinBehaviour
 
     //}
 
-    void UpdateMap()
+    public void UpdateMap(int x, int y, int value)
     {
+        level_map[x, y] = value;
+    }
 
+    public int GetValue(int x, int y)
+    {
+        return level_map[x, y];
+    }
+
+    public void GetPositionByeValue(out int x, out int y, int value)
+    {
+        for (int j = 0; j < map_height; j++)
+        {
+            for (int i = 0; i < map_width; i++)
+            {
+                if(level_map[i, j] == value)
+                {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        x = 0;
+        y = 0;
     }
 
 }
