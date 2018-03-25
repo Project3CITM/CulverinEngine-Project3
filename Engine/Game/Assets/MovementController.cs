c9a1378a122b516ac517b6a0c76809d67da4474e
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2018-03-25 21:24:32 +0200

    Scene cleared!

--- Engine/Game/Assets/MovementController.cs
+++
@@ -1,9 +0,0 @@
-﻿using CulverinEditor;
-using CulverinEditor.Debug;
-using CulverinEditor.Map;
-
-//Attach this script to the tank parent object if you want to see it rotate
-public class MovementController : CulverinBehaviour
-{
-   
-}
