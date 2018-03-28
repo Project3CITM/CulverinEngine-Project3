eb7308bd2ea1e980b075ffd310c44ea03cacce04
Carlos Cabreira Sánchez <carcasanchez@gmail.com>
2018-03-28 13:31:32 +0200

    Particles: created script to place system properly

---
+++ CulverinEditor/CulverinEditor/BarrelPuzle/SetParticleTransf.cs
@@ -0,0 +1,44 @@
+﻿using CulverinEditor;
+using CulverinEditor.Debug;
+using CulverinEditor.Map;
+
+public class SetParticleTransf : CulverinBehaviour
+{
+  
+    public GameObject wall_particle_obj;
+    public GameObject enemy_particle_obj;
+
+    CompParticleSystem wall_particle;
+    CompParticleSystem enemy_particle;
+
+
+    CompCollider col;
+
+    void Start()
+    {
+        col = GetComponent<CompCollider>();
+
+        enemy_particle = enemy_particle_obj.GetComponent<CompParticleSystem>();
+        wall_particle = wall_particle_obj.GetComponent<CompParticleSystem>();
+
+    }
+
+    void OnContact()
+    {
+        GameObject obj = col.GetCollidedObject();
+
+        if(obj.CompareTag("enemy"))
+        {
+            enemy_particle.ActivateEmission(true);
+           
+            //TODO: SET ORIENTATION
+        }
+        else if (obj.CompareTag("wall"))
+        {
+            wall_particle.ActivateEmission(true);
+            //TODO: SET ORIENTATION
+        }
+    }
+
+}
+
