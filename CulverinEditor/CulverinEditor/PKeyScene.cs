1fbe6b21b7dfaa95bd66c475cd426d935ce5eee6
xDragan <marcfv97@gmail.com>
2018-03-28 19:16:24 +0200

    Added ui scenes
    some ui scenes and placeholder font and image (pink one)

---
+++ CulverinEditor/CulverinEditor/PKeyScene.cs
@@ -0,0 +1,41 @@
+﻿using CulverinEditor;
+using CulverinEditor.SceneManagement;
+using CulverinEditor.Debug;
+
+
+class PKeyScene : CulverinBehaviour
+{
+    void Update()
+    {
+        if (Input.GetInput_KeyDown("TriangleR", "Player"))
+        {
+            SceneSwap();
+        }
+        if (Input.GetInput_KeyDown("TriangleL", "Player"))
+        {
+            SceneSwap();
+        }
+        if (Input.GetInput_KeyDown("RAllyAttack", "Player"))
+        {
+            SceneSwap();
+        }
+        if (Input.GetInput_KeyDown("LAllyAttack", "Player"))
+        {
+            SceneSwap();
+        }
+        if (Input.GetInput_KeyDown("RAttack", "Player"))
+        {
+            SceneSwap();
+        }
+        if (Input.GetInput_KeyDown("LAttack", "Player"))
+        {
+            SceneSwap();
+        }
+    }
+
+    void SceneSwap()
+    {
+        Debug.Log("SceneSwap");
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
