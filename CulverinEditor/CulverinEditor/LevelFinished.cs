b8b8c46012b0473b018005463604fb1a190e3080
Carlos Cabreira Sánchez <carcasanchez@gmail.com>
2018-04-05 20:02:19 +0200

    Game Loop completed in case debug

---
+++ CulverinEditor/CulverinEditor/LevelFinished.cs
@@ -0,0 +1,27 @@
+﻿using CulverinEditor;
+using CulverinEditor.SceneManagement;
+using CulverinEditor.Debug;
+
+
+
+   public class LevelFinished : CulverinBehaviour
+   {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                SceneManager.LoadScene("ScoreMenu");
+            }
+        }
+
+        void OnTriggerEnter()
+        {
+            GameObject obj = GetComponent<CompCollider>().GetCollidedObject();
+            if(obj != null && obj.CompareTag("player"))
+            {
+                SceneManager.LoadScene("ScoreMenu");
+            }
+        }               
+    }
+
+
