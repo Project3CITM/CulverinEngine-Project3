3efa1923f8193dcf799a32fb58c25660778f6fe6
Carlos Cabreira Sánchez <carcasanchez@gmail.com>
2018-04-05 18:52:54 +0200

    Added half of game loop

---
+++ Engine/Game/Assets/ContinueButton.cs
@@ -0,0 +1,14 @@
+﻿using CulverinEditor;
+using CulverinEditor.SceneManagement;
+using CulverinEditor.Debug;
+
+public class ContinueButton : CulverinBehaviour
+{
+    public void Play()
+    {
+        Debug.Log("GOING TO GAME");
+        Input.SetInputManagerActive("GUI", false);
+        SceneManager.LoadScene("FinalMap1");
+    }
+
+}
