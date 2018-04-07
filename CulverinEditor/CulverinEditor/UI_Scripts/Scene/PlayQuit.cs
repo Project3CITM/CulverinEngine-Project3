72ef56c83bf842b71b7e11c7766ca73243fb6967
xDragan <marcfv97@gmail.com>
2018-04-07 14:26:23 +0200

    Push fx on buttons added

--- CulverinEditor/CulverinEditor/UI_Scripts/Scene/PlayQuit.cs
+++ CulverinEditor/CulverinEditor/UI_Scripts/Scene/PlayQuit.cs
@@ -4,9 +4,17 @@ using CulverinEditor.Debug;
 
 public class PlayQuit : CulverinBehaviour
 {
+    CompAudio push;
+
+    void Start()
+    {
+        push = GetComponent<CompAudio>();
+    }
+
     public void Play()
     {
         Debug.Log("GOING TO PLAY");
+        push.PlayEvent("Push");
         SceneManager.LoadScene("HistoryScene");
     }
 
