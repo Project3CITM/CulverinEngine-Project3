15e11a31647269c0660ad2b9aebb170a416ecd01
Carlos Cabreira Sánchez <carcasanchez@gmail.com>
2018-04-08 11:46:43 +0200

    Audio: Solve loop bug

--- CulverinEditor/CulverinEditor/UI_Scripts/Scene/ContinueButton.cs
+++ CulverinEditor/CulverinEditor/UI_Scripts/Scene/ContinueButton.cs
@@ -5,11 +5,11 @@ using CulverinEditor.Debug;
 public class ContinueButton : CulverinBehaviour
 {
     public void Play()
-    {
+    {       
         StatsScore.ResetScore();
         Input.SetInputManagerActive("GUI", false);
         Debug.Log("GOING TO GAME");
-        SceneManager.RemoveNoDestroy();
+        SceneManager.RemoveNoDestroy();        
     }
 
 }
