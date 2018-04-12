dc61b60bf1d5e21ab05e699d1ca8ea21992aa1fa
Pere Rifà Planellas <pere.rifaplanellas@gmail.com>
2018-04-13 00:39:05 +0200

    Scripting: Alternate Door puzzle & Arrow collision improved

---
+++ CulverinEditor/CulverinEditor/Puzzles/Level 2/TrapPuzzle/TrapTile.cs
@@ -0,0 +1,107 @@
+﻿using CulverinEditor;
+using CulverinEditor.Debug;
+
+
+public class TrapTile : CulverinBehaviour
+{
+    enum CHANGE_STATE
+    {
+        TRAP_IDLE,
+        MOVING_UP,
+        MOVING_DOWN
+    }
+
+    public float max_height = 25.4f;
+    public float min_height = 0.0f;
+    public float speed_up = 10.0f;
+    public float speed_down = 30.0f;
+
+    public bool trap_walkable = false;
+    CHANGE_STATE curr_state = 0;
+
+    private int tile_x = 0;
+    private int tile_z = 0;
+    private int value = 0;
+
+    void Start()
+    {
+        Vector3 global_pos = transform.GetGlobalPosition();
+        tile_x = (int)((global_pos.x + (12.7f)) / 25.4);
+        tile_z = (int)((global_pos.z + (12.7f)) / 25.4);
+
+        if (trap_walkable)
+        {
+            value = 0;
+        }
+        else
+        {
+            value = 1;
+        }
+    }
+
+    void Update()
+    {
+        if (curr_state != 0)
+        {
+            if (curr_state == CHANGE_STATE.MOVING_DOWN)
+            {
+                Vector3 pos = GetComponent<Transform>().GetPosition();
+                if (pos.y > min_height)
+                {
+                    pos.y -= speed_down * Time.deltaTime;
+                    GetComponent<Transform>().SetPosition(pos);
+                }
+                else
+                {
+                    pos.y = min_height;
+                    GetComponent<Transform>().SetPosition(pos);
+                    curr_state = CHANGE_STATE.TRAP_IDLE;
+                }
+            }
+            else
+            {
+                Vector3 pos = GetComponent<Transform>().GetPosition();
+                if (pos.y < max_height)
+                {
+                    pos.y += speed_up * Time.deltaTime;
+                    GetComponent<Transform>().SetPosition(pos);
+                }
+                else
+                {
+                    pos.y = max_height;
+                    GetComponent<Transform>().SetPosition(pos);
+                    curr_state = CHANGE_STATE.TRAP_IDLE;
+                }
+            }
+        }
+    }
+
+    public bool SwitchTileState(out int curr_x, out int curr_z, out int out_value)
+    {
+        trap_walkable = !trap_walkable;
+        if (trap_walkable)
+        {
+            curr_state = CHANGE_STATE.MOVING_DOWN;
+            value = 0;
+        }
+        else
+        {
+            curr_state = CHANGE_STATE.MOVING_UP;
+            value = 1;
+        }
+
+        curr_x = tile_x;
+        curr_z = tile_z;
+        out_value = value;
+
+        return !trap_walkable;
+    }
+
+    public bool CheckChangeState(out int curr_x, out int curr_z, out int out_value)
+    {
+        curr_x = tile_x;
+        curr_z = tile_z;
+        out_value = value;
+        return (curr_state == 0 && trap_walkable);
+    }
+}
