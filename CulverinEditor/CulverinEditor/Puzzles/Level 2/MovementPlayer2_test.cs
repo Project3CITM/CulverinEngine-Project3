6eb8a3fef801a798316552f90491e6ad7d8869b9
elliotjb <elliot.jimenez1@gmail.com>
2018-04-13 10:34:34 +0200

    Fixed scene Puzzle Projectile

---
+++ CulverinEditor/CulverinEditor/Puzzles/Level 2/MovementPlayer2_test.cs
@@ -0,0 +1,41 @@
+﻿using CulverinEditor;
+using CulverinEditor.Debug;
+using CulverinEditor.Map;
+
+//Attach this script to the tank parent object if you want to see it rotate
+public class MovementPlayer2_test : CulverinBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyRepeat(KeyCode.A)) //Left
+        {
+            Vector3 temp = transform.position;
+            temp.z -= 1;
+            transform.SetPosition(temp);
+        }
+        else if (Input.GetKeyRepeat(KeyCode.D)) //Right
+        {
+            Vector3 temp = transform.position;
+            temp.z += 1;
+            transform.SetPosition(temp);
+        }
+        else if (Input.GetKeyRepeat(KeyCode.W)) //Up
+        {
+            Vector3 temp = transform.position;
+            temp.x += 1;
+            transform.SetPosition(temp);
+        }
+        else if (Input.GetKeyRepeat(KeyCode.S)) //Down
+        {
+            Vector3 temp = transform.position;
+            temp.x -= 1;
+            transform.SetPosition(temp);
+        }
+    }
+}
