c5a00a515ea66434ebd6ebd3a7ebba3710c9b63a
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2018-04-13 13:56:14 +0200

    Boss wide attack improved!

--- CulverinEditor/CulverinEditor/AI/Actions/BossWideAttack_Action.cs
+++ CulverinEditor/CulverinEditor/AI/Actions/BossWideAttack_Action.cs
@@ -14,14 +14,14 @@ public class BossWideAttack_Action : Action
         damage = dmg;
     }
 
-    public enum BASD_STATE
+    public enum BWA_STATE
     {
         WAITING,
         PRE_APPLY,
         POST_APPLY
     }
 
-    BASD_STATE state = BASD_STATE.WAITING;
+    BWA_STATE state = BWA_STATE.WAITING;
     public float damage = 1.0f;
     public float apply_damage_point = 0.5f;
     public float attack_duration = 1.0f;
@@ -30,7 +30,7 @@ public class BossWideAttack_Action : Action
     public override bool ActionStart()
     {
 
-        state = BASD_STATE.PRE_APPLY;
+        state = BWA_STATE.PRE_APPLY;
         GetComponent<CompAnimation>().SetTransition("ToAttack");
         GetComponent<CompAnimation>().SetClipDuration("Attack", attack_duration);
         return true;
@@ -39,9 +39,9 @@ public class BossWideAttack_Action : Action
     public override ACTION_RESULT ActionUpdate()
     {
 
-        if (state == BASD_STATE.PRE_APPLY && GetComponent<CompAnimation>().IsAnimOverXTime(apply_damage_point))
+        if (state == BWA_STATE.PRE_APPLY && GetComponent<CompAnimation>().IsAnimOverXTime(apply_damage_point))
         {
-            state = BASD_STATE.POST_APPLY;
+            state = BWA_STATE.POST_APPLY;
 
             int enemy_tile_x = GetComponent<Movement_Action>().GetCurrentTileX();
             int enemy_tile_y = GetComponent<Movement_Action>().GetCurrentTileY();
@@ -51,35 +51,35 @@ public class BossWideAttack_Action : Action
             switch (GetComponent<Movement_Action>().GetDirection())
             {
                 case Movement_Action.Direction.DIR_WEST:
-                    //if ((enemy_tile_x - 1 == player_tile_x || enemy_tile_x == player_tile_x) && (player_tile_y == enemy_tile_y || ))
+                    if ((enemy_tile_x - 1 == player_tile_x || enemy_tile_x == player_tile_x) && (player_tile_y == enemy_tile_y || player_tile_y == enemy_tile_y -1 || player_tile_y == enemy_tile_y + 1))
                     {
                         GetLinkedObject("player_obj").GetComponent<CharactersManager>().GetDamage(damage);
                     }
                     break;
                 case Movement_Action.Direction.DIR_EAST:
-                    if ((enemy_tile_x + 1 == player_tile_x || enemy_tile_x + 2 == player_tile_x) && player_tile_y == enemy_tile_y)
+                    if ((enemy_tile_x + 1 == player_tile_x || enemy_tile_x == player_tile_x) && (player_tile_y == enemy_tile_y || player_tile_y == enemy_tile_y - 1 || player_tile_y == enemy_tile_y + 1))
                     {
                         GetLinkedObject("player_obj").GetComponent<CharactersManager>().GetDamage(damage);
                     }
                     break;
                 case Movement_Action.Direction.DIR_NORTH:
-                    if ((enemy_tile_y - 1 == player_tile_y || enemy_tile_y - 2 == player_tile_y) && player_tile_x == enemy_tile_x)
+                    if ((enemy_tile_y - 1 == player_tile_y || enemy_tile_y == player_tile_y) && (player_tile_x == enemy_tile_y || player_tile_x == enemy_tile_y - 1 || player_tile_y == enemy_tile_x + 1))
                     {
                         GetLinkedObject("player_obj").GetComponent<CharactersManager>().GetDamage(damage);
                     }
                     break;
                 case Movement_Action.Direction.DIR_SOUTH:
-                    if ((enemy_tile_y + 1 == player_tile_y || enemy_tile_y + 2 == player_tile_y) && player_tile_x == enemy_tile_x)
+                    if ((enemy_tile_y + 1 == player_tile_y || enemy_tile_y == player_tile_y) && (player_tile_x == enemy_tile_y || player_tile_x == enemy_tile_y - 1 || player_tile_y == enemy_tile_x + 1))
                     {
                         GetLinkedObject("player_obj").GetComponent<CharactersManager>().GetDamage(damage);
                     }
                     break;
             }
         }
-        else if (state == BASD_STATE.POST_APPLY && GetComponent<CompAnimation>().IsAnimationStopped("Attack"))
+        else if (state == BWA_STATE.POST_APPLY && GetComponent<CompAnimation>().IsAnimationStopped("Attack"))
         {
 
-            state = BASD_STATE.WAITING;
+            state = BWA_STATE.WAITING;
             return ACTION_RESULT.AR_SUCCESS;
         }
         return ACTION_RESULT.AR_IN_PROGRESS;
