78b072c197e9e3f7b05ce733f347de7aa50ed4be
Carlos Cabreira Sánchez <carcasanchez@gmail.com>
2018-04-13 13:01:13 +0200

    Tested reverb areas and solved bug in audios

--- CulverinEditor/CulverinEditor/Miscellanea/ReverbCorridor.cs
+++ CulverinEditor/CulverinEditor/Miscellanea/ReverbCorridor.cs
@@ -6,7 +6,7 @@ public class ReverbCorridor: CulverinBehaviour
 {
 
     public float in_value = 0.0f;
-    public float out_value = 0.0f;
+  //  public float out_value = 0.0f;
     public string bus_name = "Corridor";
 
     CompCollider col;
@@ -19,19 +19,33 @@ public class ReverbCorridor: CulverinBehaviour
 
     void OnTriggerEnter()
     {
-        Debug.Log("[green] REVERB IN");
+        col = GetComponent<CompCollider>();
         GameObject col_obj = col.GetCollidedObject();
+        CompAudio col_audio = null;
+        if (col_obj != null)
+        {
+            col_audio = col_obj.GetComponent<CompAudio>();
+        }
+        else
+        {
+            Debug.Log("[red] COL_OBJ IS NULL!");
+        }
 
-        CompAudio col_audio = col_obj.GetComponent<CompAudio>();
         if (col_audio != null)
         {
+            Debug.Log("[green] REVERB IN");
+            Debug.Log(in_value);
             col_audio.SetAuxiliarySends(bus_name, in_value);
+        }
+        else
+        {
+            Debug.Log("[red] COL_AUDIO IS NULL!");
         }
 
     }
 
 
-    void OnTriggerLost()
+    /*void OnTriggerLost()
     {
         Debug.Log("[green] REVERB OUT");
         GameObject col_obj = col.GetCollidedObject();
@@ -44,6 +58,6 @@ public class ReverbCorridor: CulverinBehaviour
         {
             col_audio.SetAuxiliarySends(bus_name, out_value);
         }
-    }
+    }*/
 
 }
