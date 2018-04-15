b2545e31d48c2e2a1b72567e2ddd281559a19677
elliotjb <elliot.jimenez1@gmail.com>
2018-04-15 14:35:17 +0200

    Engine: Fixed bug with get variables

--- Engine/CSharpScript.h
+++ Engine/CSharpScript.h
@@ -88,6 +88,12 @@ private:
 
 };
 
+struct XaviStruct
+{
+	MonoClassField* mono_field = nullptr;
+	MonoType* mono_type = nullptr;
+};
+
 class ScriptVariable
 {
 public:
@@ -361,7 +367,8 @@ public:
 private:
 
 	//Auxiliar map to fill variables vector with info
-	std::map<MonoClassField*, MonoType*> field_type;
+	std::multimap<uint, XaviStruct> field_type;
+	uint count_variables = 0;
 	std::string nameCSharp;
 	std::string name_space;
 
