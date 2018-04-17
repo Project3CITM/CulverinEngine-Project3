d738a24a522bb290052380ee54831c3e1a4b5a7e
Danny0ner <dlopez3a@gmail.com>
2018-04-17 17:12:47 +0200

    New firebreath now lasts until it hits a wall

--- CulverinEditor/CulverinEditor/Player/Daenerys/Fireball.cs
+++ CulverinEditor/CulverinEditor/Player/Daenerys/Fireball.cs
@@ -9,23 +9,31 @@ public class Fireball : CulverinBehaviour
     public Vector3 vfront = Vector3.Zero;
     public GameObject fireball_particles;
     private CompRigidBody rb;
+    public float vel = 50;
     private float timer = 0.0f;
-
+    public bool rotate = true;
     void Start()
     {
         rb = GetComponent<CompRigidBody>();
+        if(vfront == Vector3.Zero)
+        {
+            vfront = transform.GetBackwardVector();
+        }
         Shoot();
         damage = 10.0f;
         timer = 0.0f;
         life = 5.0f;
-
-        GetComponent<CompAudio>().PlayEvent("DaenerysFire"); //Change This!!
+        
+        //GetComponent<CompAudio>().PlayEvent("DaenerysFire"); //Change This!!
     }
 
     public void Shoot()
     {
-        rb.ApplyImpulse(vfront * 50); // Forward impulse
-        rb.ApplyTorqueForce(new Vector3(0, 0, 40)); // Fall force
+        rb.ApplyImpulse(vfront * vel); // Forward impulse
+        if(rotate)
+        {
+            rb.ApplyTorqueForce(new Vector3(0, 0, 40));
+        }
     }
 
     void Update()
