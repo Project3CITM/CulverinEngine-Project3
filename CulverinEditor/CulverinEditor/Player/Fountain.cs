2f2f694922b9183b74c5a612300322eb1b77b789
Adrian Castillo <adriancl1996@gmail.com>
2018-04-18 00:36:51 +0200

    Now fountain has limited uses and deplenishes its water

--- CulverinEditor/CulverinEditor/Player/Fountain.cs
+++ CulverinEditor/CulverinEditor/Player/Fountain.cs
@@ -7,22 +7,95 @@ public class Fountain : CulverinBehaviour
 {
     public GameObject fountain_interact;
     public GameObject player;
+    public GameObject upper_water;
+    public GameObject bottom_water;
+    public int number_of_uses = 3;
+    public float final_y_bottom = 0;
+    public float final_x_scale_bottom = 0;
+    public float final_z_scale_bottom = 0;
+    public float deplenish_speed = 0.01f;
+    private float starting_y_bottom;
+    private float starting_x_scale_bottom;
+    private float starting_z_scale_bottom;
+    private float destination_y = 0;
+    private float destination_x = 0;
+    private float destination_z = 0;
+    private int current_usage = 0;
+    private bool reducing_water = false;
     private bool on_fountain_range = false;
 
     void Start()
     {
         player = GetLinkedObject("player");
         fountain_interact = GetLinkedObject("fountain_interact");
+        upper_water = GetLinkedObject("upper_water");
+        bottom_water = GetLinkedObject("bottom_water");
         on_fountain_range = false;
+        current_usage = number_of_uses;
+        reducing_water = false;
+        starting_x_scale_bottom = bottom_water.transform.GetScale().x;
+        starting_z_scale_bottom = bottom_water.transform.GetScale().z;
+        starting_y_bottom = bottom_water.transform.GetPosition().y;
     }
 
     void Update()
     {
-        if (on_fountain_range)
+        if (on_fountain_range == true)
         {
-            if (Input.GetInput_KeyDown("Interact", "Player"))
+            if (Input.GetInput_KeyDown("Interact", "Player") && current_usage > 0)
             {
                 GetLinkedObject("player").GetComponent<CharactersManager>().HealCharacters();
+                reducing_water = true;
+                current_usage--;
+                float by = (float)current_usage / (float)number_of_uses;
+                destination_y = Mathf.Lerp(starting_y_bottom, final_y_bottom, by);
+                Debug.Log(destination_y, Department.STAGE, Color.PINK);
+                Debug.Log(starting_y_bottom, Department.STAGE, Color.ORANGE);
+                Debug.Log(final_y_bottom, Department.STAGE, Color.GREEN);
+                Debug.Log(by, Department.STAGE, Color.BLUE);
+                destination_x = Mathf.Lerp(starting_x_scale_bottom, final_x_scale_bottom, by);
+                destination_z = Mathf.Lerp(starting_z_scale_bottom, final_z_scale_bottom, by);
+            }
+        }
+
+        if(reducing_water == true)
+        {
+            int checks = 0;
+            Vector3 bottom_pos = GetLinkedObject("bottom_water").transform.GetPosition();
+            if(destination_y < bottom_pos.y)
+            {
+                Debug.Log(bottom_pos.y, Department.STAGE);
+                Debug.Log("Going toooo", Department.STAGE);
+                Debug.Log(destination_y, Department.STAGE);
+                bottom_water.transform.SetPosition(new Vector3(bottom_pos.x, bottom_pos.y - deplenish_speed, bottom_pos.z));
+            }
+            else
+            {
+                checks++;
+            }
+
+            Vector3 bottom_scale = bottom_water.transform.GetScale();
+            if (destination_x < bottom_scale.x)
+            {
+                bottom_water.transform.SetScale(new Vector3(bottom_scale.x - deplenish_speed, bottom_scale.y, bottom_scale.z));
+                bottom_scale.x -= Time.deltaTime * 2;
+            }
+            else
+            {
+                checks++;
+            }
+            if (destination_z < bottom_scale.z)
+            {
+                bottom_water.transform.SetScale(new Vector3(bottom_scale.x, bottom_scale.y, bottom_scale.z - deplenish_speed));
+            }
+            else
+            {
+                checks++;
+            }
+
+            if (checks == 3)
+            {
+                reducing_water = false;
             }
         }
     }
@@ -34,17 +107,18 @@ public class Fountain : CulverinBehaviour
         GameObject obj_col = col.GetCollidedObject();
         Debug.Log(obj_col.GetTag().ToString());
 
-        if (obj_col != null && obj_col.CompareTag("player"))
+        if (obj_col != null && obj_col.CompareTag("player") && current_usage > 0)
         {
             fountain_interact = GetLinkedObject("fountain_interact");
             fountain_interact.SetActive(true);
             on_fountain_range = true;
-
         }
     }
 
     void OnTriggerLost()
     {
         on_fountain_range = false;
+        fountain_interact = GetLinkedObject("fountain_interact");
+        fountain_interact.SetActive(true);
     }
 }
