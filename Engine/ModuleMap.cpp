e1ff1592f0348fa6286aff2e0ffd0b97ae9f2a63
elliotjb <elliot.jimenez1@gmail.com>
2018-04-18 16:35:27 +0200

    Engine: Fixed Editor Map

--- Engine/ModuleMap.cpp
+++ Engine/ModuleMap.cpp
@@ -566,12 +566,17 @@ void ModuleMap::ShowCreationMap()
 			ImGui::Text("%i: ", i + 1); ImGui::SameLine();
 			if (ImGui::BeginCombo("##Pref", prefabs[i].c_str()))
 			{
+				static ImGuiTextFilter filter;
+				filter.Draw();
 				for (int n = 0; n < all_prefabs.size(); n++)
 				{
-					if (ImGui::Selectable(all_prefabs[n].c_str()))
+					if (filter.PassFilter(all_prefabs[n].c_str()))
 					{
-						prefabs[i] = all_prefabs[n];
-						ImGui::SetItemDefaultFocus();//
+						if (ImGui::Selectable(all_prefabs[n].c_str()))
+						{
+							prefabs[i] = all_prefabs[n];
+							ImGui::SetItemDefaultFocus();//
+						}
 					}
 				}
 				ImGui::EndCombo();
@@ -675,6 +680,16 @@ void ModuleMap::ShowCreationMap()
 				if (map[x][y] > -1 && force_pop)
 				{
 					ImGui::PopStyleColor();
+				}
+				std::string position_tile = "x:" + std::to_string(x);
+				position_tile += " - y:" + std::to_string(y);
+				if (ImGui::IsItemHovered())
+				{
+					ImGui::BeginTooltip();
+					ImGui::PushTextWrapPos(450.0f);
+					ImGui::TextUnformatted(position_tile.c_str());
+					ImGui::PopTextWrapPos();
+					ImGui::EndTooltip();
 				}
 				if (ImGui::BeginPopupContextItem("Options##maptile3d"))
 				{
@@ -988,7 +1003,6 @@ void ModuleMap::ShowNavigationMap()
 
 void ModuleMap::OptionsTile(int x, int y)
 {
-	ImGui::Text("%i,%i", x, y);
 	if (ImGui::MenuItem("Delete"))
 	{
 		map[x][y] = -1;
