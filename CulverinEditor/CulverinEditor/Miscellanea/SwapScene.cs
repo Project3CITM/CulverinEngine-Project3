03cfe03d13a8b1cb55106d4845548bc30a310ac7
Josef21296 <jcasanovasv@hotmail.com>
2018-04-26 16:41:13 +0200

    Swap scene script for metrics testing

---
+++ CulverinEditor/CulverinEditor/Miscellanea/SwapScene.cs
@@ -0,0 +1,25 @@
+﻿using CulverinEditor;
+using CulverinEditor.SceneManagement;
+using CulverinEditor.Debug;
+
+public class SwapScene : CulverinBehaviour
+{
+    public string sceneName = "MetricsTestScene";
+    public int swapSceneIndexSuf = 0;
+
+    private string nextSceneName = "";
+
+    void Start()
+    {
+        nextSceneName = sceneName + swapSceneIndexSuf.ToString();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("Changing scene to: " + nextSceneName);
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
