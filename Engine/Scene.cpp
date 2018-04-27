bf1e19ffc6c87fbf6bbd7774abc42b206d742a7c
Pere Rifà Planellas <pere.rifaplanellas@gmail.com>
2018-04-27 10:40:44 +0200

    Engine: Fixed minor bugs

--- Engine/Scene.cpp
+++ Engine/Scene.cpp
@@ -1167,6 +1167,7 @@ GameObject* Scene::CreateGameObject(GameObject* parent)
 	if (parent == nullptr)
 	{
 		root->AddChildGameObject(obj);
+		dynamic_objects.push_back(obj);
 	}
 
 	return obj;
@@ -1547,6 +1548,7 @@ GameObject * Scene::CreateMainLight(GameObject * parent)
 	{
 		// Only add to GameObjects list the Root Game Objects
 		App->scene->root->AddChildGameObject(obj);
+		dynamic_objects.push_back(obj);
 	}
 
 	LOG("MAIN CAMERA Created.");
