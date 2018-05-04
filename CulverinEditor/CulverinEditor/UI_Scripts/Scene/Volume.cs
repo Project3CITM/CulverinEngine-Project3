857399528c674439d81a0bd227f15ae93a0ed379
Andreu <Andreu.rbj@gmail.com>
2018-05-04 14:53:48 +0200

    UI Volume.cs Update

--- CulverinEditor/CulverinEditor/UI_Scripts/Scene/Volume.cs
+++ CulverinEditor/CulverinEditor/UI_Scripts/Scene/Volume.cs
@@ -10,15 +10,20 @@ public class Volume : CulverinBehaviour
     public GameObject mute_cb;
     public bool mute = false;
     public float audio_multiplier = 100;
-
+    bool update = false;
     void Start()
     {
+        update = true;
         audio_sl = GetLinkedObject("audio_sl");
         mute_cb = GetLinkedObject("mute_cb");
     }
 
     void Update()
     {
+        if(!update)
+        {
+            Start();
+        }
         SetVolume();
     }
 
