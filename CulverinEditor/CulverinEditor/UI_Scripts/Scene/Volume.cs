7289318f90cb425dddef836608fbce00e78d6083
Andreu <Andreu.rbj@gmail.com>
2018-05-04 14:57:35 +0200

    UI Change Volumen Funcion polish

--- CulverinEditor/CulverinEditor/UI_Scripts/Scene/Volume.cs
+++ CulverinEditor/CulverinEditor/UI_Scripts/Scene/Volume.cs
@@ -10,12 +10,19 @@ public class Volume : CulverinBehaviour
     public GameObject mute_cb;
     public bool mute = false;
     public float audio_multiplier = 100;
+    float value = 0.0f;
+    float last_value = 0.0f;
+
     bool update = false;
     void Start()
     {
         update = true;
         audio_sl = GetLinkedObject("audio_sl");
         mute_cb = GetLinkedObject("mute_cb");
+        value = audio_sl.GetComponent<CompSlider>().GetFill();
+        Audio.ChangeVolume(value * audio_multiplier);
+        last_value = value;
+
     }
 
     void Update()
@@ -29,8 +36,11 @@ public class Volume : CulverinBehaviour
 
     public void SetVolume()
     {
-        float multiplier = audio_sl.GetComponent<CompSlider>().GetFill();
-        Audio.ChangeVolume(multiplier * audio_multiplier);
+        value = audio_sl.GetComponent<CompSlider>().GetFill();
+        if (value == last_value)
+            return;
+        Audio.ChangeVolume(value * audio_multiplier);
+        last_value = value;
     }
-   
+
 }
