7c6f0ad7766d8f8a0d4418e23915cc72b979ccbe
Marc Latorre <marc.lafrwy@gmail.com>
2018-05-04 11:18:47 +0200

    Boss: Hp bar appearing with a fade

--- CulverinEditor/CulverinEditor/AI/BossHPBar.cs
+++ CulverinEditor/CulverinEditor/AI/BossHPBar.cs
@@ -7,15 +7,41 @@ public class BossHPBar : CulverinBehaviour
     public GameObject hp_bar_boss_image = null;
     public GameObject hp_bar_boss_text = null;
 
-    public float hp_bar_alpha = 0.75f;
+    public float hp_bar_appearing_time = 2.0f;
+    private float hp_bar_appearing_timer = 0.0f;
+    private bool appeared = false;
+
+    private CompImage hp_bar_img;
+    private CompImage hp_bar_img_background;
 
     public void Start()
     {
         hp_bar_boss_background = GetLinkedObject("hp_bar_boss_background");
         hp_bar_boss_image = GetLinkedObject("hp_bar_boss_image");
         hp_bar_boss_text = GetLinkedObject("hp_bar_boss_text");
-        hp_bar_boss_background.GetComponent<CompImage>().SetAlpha(hp_bar_alpha);
-        GetComponent<CompImage>().SetAlpha(hp_bar_alpha);
+        hp_bar_img = hp_bar_boss_background.GetComponent<CompImage>();
+        hp_bar_img_background = GetComponent<CompImage>();
+
+        hp_bar_img.SetAlpha(0.0f);
+        hp_bar_img_background.SetAlpha(0.0f);
+        hp_bar_appearing_timer = 0.0f;
+        appeared = false;
+    }
+
+    public void Update()
+    {
+        if (!appeared)
+        {
+            hp_bar_appearing_timer += Time.deltaTime;
+            hp_bar_img.SetAlpha(hp_bar_appearing_timer / hp_bar_appearing_time);
+            hp_bar_img_background.SetAlpha(hp_bar_appearing_timer / hp_bar_appearing_time);
+            if (hp_bar_appearing_timer >= hp_bar_appearing_time)
+            {
+                hp_bar_img.SetAlpha(1.0f);
+                hp_bar_img_background.SetAlpha(1.0f);
+                appeared = true;
+            }
+        }
     }
 
     public void ActivateHPBar(bool active)
@@ -23,7 +49,6 @@ public class BossHPBar : CulverinBehaviour
         hp_bar_boss_background.SetActive(active);
         hp_bar_boss_image.SetActive(active);
         hp_bar_boss_text.SetActive(active);
-
     }
 
     public void SetHPBar(float hp_percentage)
