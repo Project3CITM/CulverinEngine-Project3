485de273cd3d569f21ef6c95affef6bda19dc88f
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2018-05-05 14:52:49 +0200

    Soldier dies trigger added!

---
+++ CulverinEditor/CulverinEditor/AI/SoldierDiesTrigger.cs
@@ -0,0 +1,19 @@
+﻿using CulverinEditor;
+using CulverinEditor.Debug;
+using CulverinEditor.SceneManagement;
+using System;
+
+public class SoldierDiesTrigger : CulverinBehaviour
+{
+    bool triggered = false;
+
+    void OnTriggerEnter()
+    {
+        if(triggered == false)
+        {
+            triggered = true;
+            Debug.Log("GRITOOO!!");
+            GetComponent<CompAudio>().PlayEvent("BossIntro");
+        }
+    }
+}
