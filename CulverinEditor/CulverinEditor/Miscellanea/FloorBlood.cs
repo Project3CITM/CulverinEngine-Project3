b4739844363a30cef3efe19d078e7d7c73c2465b
Jordior97 <jordior97@gmail.com>
2018-05-05 21:07:43 +0200

    Fixed blood when enemy dies

---
+++ CulverinEditor/CulverinEditor/Miscellanea/FloorBlood.cs
@@ -0,0 +1,29 @@
+﻿using CulverinEditor;
+using CulverinEditor.Debug;
+using CulverinEditor.Map;
+using CulverinEditor.SceneManagement;
+
+public class FloorBlood : CulverinBehaviour
+{
+    public float max_expansion = 1.0f;
+    public float expansion_factor = 1.0f;
+    Vector3 scale;
+
+    void Start()
+    {
+        scale = transform.GetScale();
+        Debug.Log(scale, Department.PLAYER, Color.RED);
+    }
+
+    void Update()
+    {
+        if (scale.x < max_expansion) 
+        {
+            scale.x += Time.deltaTime * (expansion_factor / 10.0f);
+            scale.z += Time.deltaTime * (expansion_factor / 10.0f);
+
+            transform.SetScale(scale);
+            Debug.Log(scale, Department.PLAYER, Color.RED);
+        }
+    }
+}
