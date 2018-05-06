3cd58382888997dedfd6f3de886b4cd95d2c8ae8
Joan Pareja <imbecilustotalus@gmail.com>
2018-05-06 13:20:26 +0200

    UI: Updated score script

--- CulverinEditor/CulverinEditor/UI_Scripts/ShowScore2.cs
+++ CulverinEditor/CulverinEditor/UI_Scripts/ShowScore2.cs
@@ -5,6 +5,18 @@ using CulverinEditor.Debug;
 
 public class ShowScore2 : CulverinBehaviour
 {
+
+    public GameObject time_title;
+    public GameObject time_title_gold;
+    public GameObject survivors_title;
+    public GameObject survivors_title_gold;
+    public GameObject combat_title;
+    public GameObject combat_title_gold;
+    public GameObject skill_level_title;
+    public GameObject skill_level_title_gold;
+    public GameObject grade_title;
+    public GameObject grade_title_gold;
+
     public GameObject time_text;
     public GameObject survivors_count;
     public GameObject combat_point;
@@ -31,6 +43,20 @@ public class ShowScore2 : CulverinBehaviour
 
     void Start()
     {
+        time_title = GetLinkedObject("time_title");
+        time_title_gold = GetLinkedObject("time_title_gold");
+        survivors_title = GetLinkedObject("survivors_title");
+        survivors_title_gold = GetLinkedObject("survivors_title_gold");
+        combat_title = GetLinkedObject("combat_title");
+        combat_title_gold = GetLinkedObject("combat_title_gold");
+        skill_level_title = GetLinkedObject("skill_level_title");
+        skill_level_title_gold = GetLinkedObject("skill_level_title_gold");
+
+        time_title_gold.SetActive(false);
+        survivors_title_gold.SetActive(false);
+        combat_title_gold.SetActive(false);
+        skill_level_title_gold.SetActive(false);
+
         time_text = GetLinkedObject("time_text");
         survivors_count = GetLinkedObject("survivors_count");
         combat_point = GetLinkedObject("combat_point");
@@ -78,16 +104,33 @@ public class ShowScore2 : CulverinBehaviour
         string text = minutes.ToString("D2") + ":" + seconds.ToString("D2");
 
         time_text.GetComponent<CompText>().SetText(text);
+
+        if(StatsScore.time < gold_time)
+        {
+            time_title.SetActive(false);
+            time_title_gold.SetActive(true);
+        }
+
     }
 
     void DisplaySurvivors()
     {
         survivors_count.GetComponent<CompText>().SetText(StatsScore.characters_alive.ToString() + "/3");
+        if(StatsScore.characters_alive == 3)
+        {
+            survivors_title.SetActive(false);
+            survivors_title_gold.SetActive(true);
+        }
     }
 
     void DisplayCombatPoints()
     {
         combat_point.GetComponent<CompText>().SetText(StatsScore.combat_points.ToString() + "/500");
+        if(StatsScore.combat_points > gold_combat_points)
+        {
+            combat_title.SetActive(false);
+            combat_title_gold.SetActive(true);
+        }
     }
 
     void DisplaySkillLevel()
@@ -103,6 +146,8 @@ public class ShowScore2 : CulverinBehaviour
         if (skill_coins == 3)
         {
             skill_gold_coin.SetActive(true);
+            skill_level_title.SetActive(false);
+            skill_level_title_gold.SetActive(true);
         }
     }
 
@@ -112,6 +157,8 @@ public class ShowScore2 : CulverinBehaviour
         {
             Debug.Log("Grade S", Department.GENERAL, Color.PINK);
             grade_mark_S.SetActive(true);
+            grade_title.SetActive(false);
+            grade_title_gold.SetActive(true);
         }
         else if (grade_points < 400 && grade_points >= 350)
         {
