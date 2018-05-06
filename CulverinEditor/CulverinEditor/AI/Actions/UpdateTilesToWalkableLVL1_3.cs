82a9452ceea469a30d5803f3debaee308f55585e
nicobabot <nico.babotaltisen@gmail.com>
2018-05-06 18:19:14 +0200

    Walkability lvl 1_3 enemy updated

---
+++ CulverinEditor/CulverinEditor/AI/Actions/UpdateTilesToWalkableLVL1_3.cs
@@ -0,0 +1,18 @@
+﻿using CulverinEditor;
+using CulverinEditor.Debug;
+using System.Collections.Generic;
+
+public class UpdateTilesToWalkableLVL1_3 : CulverinBehaviour
+{
+    
+    void Start()
+    {
+        GameObject map = GetLinkedObject("map_obj");
+        LevelMap map_level = map.GetComponent<LevelMap>();
+        if (map_level != null)
+        {
+            map_level.UpdateMap(13, 7, 0);
+        }
+    }
+  
+}
